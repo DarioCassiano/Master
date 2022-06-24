b53cfd6ef3dbb276475a2007633cd63ff731a3c6
Dario Cassiano <contatodariocassiano@gmail.com>
2022-06-23 22:30:50 -0300

    Desenvolvimento Framework banco original

---
+++ src/test/java/Metodo/Metodo.java
@@ -0,0 +1,5 @@
+package Metodo;
+
+public class Metodo {
+
+}
---
+++ src/test/java/Steps/Steps.java
@@ -0,0 +1,48 @@
+package Steps;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class Steps {
+
+	@Given("^que acesse a Url \"([^\"]*)\"$")
+	public void que_acesse_a_Url(String arg1) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^selecionar o botao abrir nova conta$")
+	public void selecionar_o_botao_abrir_nova_conta() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^selecione o  botao abrir conta pf$")
+	public void selecione_o_botao_abrir_conta_pf() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^preencho o campo nome \"([^\"]*)\"$")
+	public void preencho_o_campo_nome(String arg1) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^seleciono o botao quero ser cliente$")
+	public void seleciono_o_botao_quero_ser_cliente() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^realizo validacao de envio dos dados$")
+	public void realizo_validacao_de_envio_dos_dados() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+
+	
+}
